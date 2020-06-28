7d48743e7f1756f12128fe92deeaad703f96b288
Luiz Nunes <lznunes@gmail.com>
2020-06-28 20:29:13 -0300

    Incluido template para inserção de evento

--- core/models.py
+++ core/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 class Evento(models.Model):
     titulo = models.CharField(max_length=200)
     descricao = models.TextField(blank=True, null=True)
-    data_evento = models.DateField(verbose_name='Data do Evento')
-    data_criacao = models.DateField(auto_now=True)
+    data_evento = models.DateField(verbose_name='Data do Evento', null=True)
+    data_criacao = models.DateField(auto_now=True, null=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+import time
 # Create your views here.
 
 #def index(request):
@@ -27,6 +27,10 @@ def logout_user(request):
     return redirect('/agenda/')
 
 @login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
     #evento = Evento.objects.get(id=1)
     usuario = request.user
@@ -36,3 +40,16 @@ def lista_eventos(request):
     dados = {'eventos' : evento}
     return render(request, 'agenda.html', dados)
 
+@login_required(login_url='/login')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario
+                              )
+    return redirect('/')
