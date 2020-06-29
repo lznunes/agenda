f17e90fcdaf912ac82228d9b63d48bd5a781b7b1
Luiz Nunes <lznunes@gmail.com>
2020-06-29 03:44:18 -0300

    Inicio do processo de update da agenda

--- core/models.py
+++ core/models.py
@@ -7,7 +7,7 @@ class Evento(models.Model):
     titulo = models.CharField(max_length=200)
     descricao = models.TextField(blank=True, null=True)
     local = models.TextField(blank=True, null=True, max_length=50)
-    data_evento = models.DateField(verbose_name='Data do Evento', null=True)
+    data_evento = models.DateField(verbose_name='Data do Evento', blank=True, null=True)
     data_criacao = models.DateField(auto_now=True, null=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -18,3 +18,6 @@ class Evento(models.Model):
         return self.titulo
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H%M')
